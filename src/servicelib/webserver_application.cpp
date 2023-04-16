1b67cfb79cbbe4074e4077a58a72499e2d20627b
Sanveg Rane <ssrane2@ncsu.edu>
2023-04-16 18:35:51 -0400

    Fixes

--- src/servicelib/webserver_application.cpp
+++ src/servicelib/webserver_application.cpp
@@ -9,8 +9,16 @@ namespace webserver
 
 Application::Application(const std::string& appName)
     : d_appName(appName)
+{}
+
+void Application::start()
 {
-    BOOST_LOG_TRIVIAL(info) << "Initializing Application: " << d_appName;
+    BOOST_LOG_TRIVIAL(info) << "Starting application " << d_appName;
+}
+
+void Application::stop() noexcept
+{
+    BOOST_LOG_TRIVIAL(info) << "Stopping application " << d_appName;
 }
 
 } // namespace webserver
